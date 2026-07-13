6510927c44712b000846c8b4ba69b2b1e05102fc
Tommaso Giovanni Baffetti (tommaso.baffetti@ulb.be) <baffetti@dragon2.umons.ac.be>
2026-07-13 20:00:42 +0200

    feat+fix: new docker image addes + updated gitignore

--- organizer_scripts/RUN.sh
+++ organizer_scripts/RUN.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=prepare_data
-#SBATCH --output=logs/prepare_data.out
-#SBATCH --error=logs/prepare_data.err
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=logs/prepare_data_%j.out
+#SBATCH --error=logs/prepare_data_%j.err
 
 #SBATCH --partition=batch
-#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --time=05:00:00
@@ -14,8 +12,7 @@
 echo "Start:"
 date
 
-module load Python/3.11.3
-source /globalsc/ulb/atm/baffetti/envs/apriori/bin/activate
+source ~/envs/cypher2026/bin/activate
 
 python3 -u prepare_data.py
 
